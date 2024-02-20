baeeb04cf6b04a8e4d5648a70ec5aca420fc1493
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2024-02-20 18:54:18 -0500

    Initial commit

---
+++ Lights.java
@@ -0,0 +1,11 @@
+package facade;
+
+public class Lights {
+    public void on() { 
+    	System.out.println("Turning lights on"); 
+    }
+    
+    public void off() { 
+    	System.out.println("Turning lights off"); 
+    }
+}
